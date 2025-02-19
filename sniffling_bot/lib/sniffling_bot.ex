75823449fab4f2c73dbbdf8600a469540a027c8b
Pofay <gianCarlo.gilos@gmail.com>
2025-02-19 09:57:58 +0800

    feat(incomplete): BSetup slash command for configuration

--- sniffling_bot/lib/sniffling_bot.ex
+++ sniffling_bot/lib/sniffling_bot.ex
@@ -17,23 +17,52 @@ defmodule SnifflingBot.Consumer do
   use Nostrum.Consumer
 
   alias Nostrum.Api.Message
+  alias Nostrum.Api.ApplicationCommand
+  alias Nostrum.Api.Interaction
+  alias Nostrum.Snowflake
 
-  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
-    case msg.content do
-      "!sleep" ->
-        Message.create(msg.channel_id, "Going to sleep...")
-        # This won't stop other events from being handled.
-        Process.sleep(3000)
+  @commands [
+    {"configure", "Configure the bot with the current discord user and github access token.",
+     [
+       %{
+         name: "access_token",
+         description: "The access token to use for github.",
+         type: 3,
+         required: true
+       }
+     ]}
+  ]
 
-      "!ping" ->
-        Message.create(msg.channel_id, "pyongyang!")
+  def handle_event({:READY, %{guilds: guilds} = _data, _ws_state}) do
+    guilds
+    |> Enum.map(fn guild -> guild.id end)
+    |> Enum.each(&create_guild_commands/1)
+  end
 
-      "!raise" ->
-        # This won't crash the entire Consumer.
-        raise "No problems here!"
+  def create_guild_commands(guild_id) do
+    Enum.each(@commands, fn {name, description, options} ->
+      ApplicationCommand.create_guild_command(guild_id, %{
+        name: name,
+        description: description,
+        options: options
+      })
+    end)
+  end
 
-      _ ->
-        :ignore
-    end
+  def handle_event({:INTERACTION_CREATE, interaction, _ws_state}) do
+    message =
+      case do_command(interaction) do
+        {:msg, msg} -> msg
+        _ -> ":white_check_mark:"
+      end
+
+    Interaction.create_response(interaction, %{type: 4, data: %{content: message}})
+  end
+
+  def do_command(%{user: user, data: %{options: options} = interaction}) do
+    IO.inspect(interaction)
+    IO.inspect(user)
+    IO.inspect(options)
+    {:msg, "Test configuration"}
   end
 end
