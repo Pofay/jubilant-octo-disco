c868ac62e30f258a5b5ddd8058a9362eff9e83aa
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-05-15 09:35:51 +0800

    Added vanilla binary search

--- binary_search_practice/test/binary_search_practice_test.exs
+++ binary_search_practice/test/binary_search_practice_test.exs
@@ -1,5 +1,5 @@
 defmodule BinarySearchPracticeTest do
-  alias BinarySearchPractice.PerfectSquare
+  alias BinarySearchPractice.{PerfectSquare, StandardBinarySearch}
   use ExUnit.Case
 
   test "is_perfect_square/1 happy paths" do
@@ -8,5 +8,13 @@ defmodule BinarySearchPracticeTest do
     assert PerfectSquare.is_perfect_square(4) == true
     assert PerfectSquare.is_perfect_square(49) == true
     assert PerfectSquare.is_perfect_square(50) == false
+  end
+
+  test "vanilla binary search happy path" do
+    assert StandardBinarySearch.binary_search([1,2,3,4,5], 1) == 0
+  end
+
+  test "vanilla binary search sad path" do
+    assert StandardBinarySearch.binary_search([1,2,3,4,5], 6) == -1
   end
 end
