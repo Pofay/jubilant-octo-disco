3d6e4f4df62d5e76d4eeb93be23d369c3efb7900
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-05-15 09:57:27 +0800

    Added first true in sorted boolean array

--- binary_search_practice/test/binary_search_practice_test.exs
+++ binary_search_practice/test/binary_search_practice_test.exs
@@ -1,5 +1,10 @@
 defmodule BinarySearchPracticeTest do
-  alias BinarySearchPractice.{PerfectSquare, StandardBinarySearch}
+  alias BinarySearchPractice.{
+    PerfectSquare,
+    StandardBinarySearch,
+    FindTrueInSortedBooleanList
+  }
+
   use ExUnit.Case
 
   test "is_perfect_square/1 happy paths" do
@@ -24,14 +29,32 @@ defmodule BinarySearchPracticeTest do
   end
 
   test "vanilla binary search happy path" do
-    assert StandardBinarySearch.binary_search([1,2,3,4,5], 1) == 0
+    assert StandardBinarySearch.binary_search([1, 2, 3, 4, 5], 1) == 0
   end
 
   test "vanilla binary search sad path" do
-    assert StandardBinarySearch.binary_search([1,2,3,4,5], 6) == -1
+    assert StandardBinarySearch.binary_search([1, 2, 3, 4, 5], 6) == -1
   end
 
   test "vanilla binary search empty list" do
     assert StandardBinarySearch.binary_search([], 1) == -1
+  end
+
+  test "find_boundary/1 empty list should return -1" do
+    assert FindTrueInSortedBooleanList.find_boundary([]) == -1
+  end
+
+  test "find_boundary/1 list with no true values should return -1" do
+    assert FindTrueInSortedBooleanList.find_boundary([false, false, false]) == -1
+  end
+
+  test "find_boundary/1 should return the index of first true value" do
+    assert FindTrueInSortedBooleanList.find_boundary([false, false, true]) == 2
+    assert FindTrueInSortedBooleanList.find_boundary([false, true, true]) == 1
+    assert FindTrueInSortedBooleanList.find_boundary([true, true, true]) == 0
+  end
+
+  test "find_boundary/1 sanity test" do
+    assert FindTrueInSortedBooleanList.find_boundary([false, false, false, true, true, true, true]) == 3
   end
 end
