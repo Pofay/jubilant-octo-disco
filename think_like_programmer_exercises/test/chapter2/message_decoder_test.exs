1c4381cf849b6d825ceb8a96555342e9e4ba6470
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-11-07 15:33:40 +0800

    feat: Implement uppercase and lowercase logic for decoder

--- think_like_programmer_exercises/test/chapter2/message_decoder_test.exs
+++ think_like_programmer_exercises/test/chapter2/message_decoder_test.exs
@@ -1,4 +1,4 @@
-defmodule ThinkLikeAProogrammerExercises.Chapter2.MessageDecoderTest do
+defmodule ThinkLikeAProogrammerExercises.Chapter2.Messagedecode_to_uppercaserTest do
     use ExUnit.Case, async: true
 
 
@@ -11,6 +11,41 @@ defmodule ThinkLikeAProogrammerExercises.Chapter2.MessageDecoderTest do
     #  |> Enum.map(fn a -> a + 64 end)
     #  |> List.to_string
     # "HX"
+    expected = "HX"
+
+    actual = Enum.map(["143", "240"], &decode_to_uppercase/1) |> List.to_string()
+
+    assert actual == expected
   end
+
+  test "Problem Reduction: Deal with Lowercase Mode" do
+    expected = "hx"
+
+    actual = Enum.map(["143", "240"], &decode_to_lowercase/1) |> List.to_string()
+
+    assert actual == expected
+  end
+
+
+  defp decode_to_uppercase(numeric_value) do
+    numeric_value
+    |> String.to_integer()
+    |> to_codepoint_upper()
+  end
+
+  defp to_codepoint_upper(numeric_value) do
+    rem(numeric_value, 27) + 64
+  end
+
+  defp decode_to_lowercase(numeric_value) do
+    numeric_value
+    |> String.to_integer()
+    |> to_codepoint_lowercase()
+  end
+
+  defp to_codepoint_lowercase(numeric_value) do
+    rem(numeric_value, 27) + 96
+  end
+
 
 end
