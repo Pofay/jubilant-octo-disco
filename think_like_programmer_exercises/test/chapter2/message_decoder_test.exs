822a05d169f7cd798292278bd12e9ee4ac1e921f
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-11-07 14:42:56 +0800

    wip: put experimentation from iex cli to test to begin formulating uppercase mode of message decoder problem

---
+++ think_like_programmer_exercises/test/chapter2/message_decoder_test.exs
@@ -0,0 +1,16 @@
+defmodule ThinkLikeAProogrammerExercises.Chapter2.MessageDecoderTest do
+    use ExUnit.Case, async: true
+
+
+  test "Problem Reduction: Deal with Uppercase Mode" do
+    # Example
+    # "143, 240"
+    #  |> String.split([","," "], trim: true)
+    #  |> Enum.map(&String.to_integer/1)
+    #  |> Enum.map(fn a -> rem(a, 27) end)
+    #  |> Enum.map(fn a -> a + 64 end)
+    #  |> List.to_string
+    # "HX"
+  end
+
+end
