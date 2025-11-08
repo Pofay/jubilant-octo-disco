60402a37fe0490c5c46137aab85fe2d7074aeb2d
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-11-09 01:08:36 +0800

    feat: Completed Message Decoder that shifts modes (Uppercase, Lowercase, Punctuation) as defined in the book

---
+++ think_like_programmer_exercises/lib/chapter_2/message_decoder.ex
@@ -0,0 +1,111 @@
+defmodule ThinkLikeAProgrammerExercises.Chapter2.MessageDecoder do
+  def decode_message(numeric_message) do
+    numeric_message
+    |> String.split([",", " "], trim: true)
+    |> Enum.reduce({[], "U"}, fn encoded_value, {decoded_message, decode_mode} ->
+      decoded_value =
+        case decode_mode do
+          "U" ->
+            decode_to_uppercase(encoded_value)
+
+          "L" ->
+            decode_to_lowercase(encoded_value)
+
+          "P" ->
+            decode_to_punctuation(encoded_value)
+        end
+
+      new_mode = get_decoding_mode(String.to_integer(encoded_value), decode_mode)
+
+      {decoded_message ++ [decoded_value], new_mode}
+    end)
+    |> elem(0)
+    |> Enum.filter(fn x -> x != 0 end)
+    |> List.to_string()
+  end
+
+  defp get_decoding_mode(value, "U") when rem(value, 27) == 0 do
+    "L"
+  end
+
+  defp get_decoding_mode(_value, "U") do
+    "U"
+  end
+
+  defp get_decoding_mode(value, "L") when rem(value, 27) == 0 do
+    "P"
+  end
+
+  defp get_decoding_mode(_value, "L") do
+    "L"
+  end
+
+  defp get_decoding_mode(value, "P") when rem(value, 9) == 0 do
+    "U"
+  end
+
+  defp get_decoding_mode(_value, "P") do
+    "P"
+  end
+
+  def decode_to_uppercase(encoded_value) do
+    encoded_value
+    |> String.to_integer()
+    |> to_codepoint_upper()
+  end
+
+  defp to_codepoint_upper(encoded_value) when rem(encoded_value, 27) == 0 do
+    0
+  end
+
+  defp to_codepoint_upper(encoded_value) do
+    rem(encoded_value, 27) + 64
+  end
+
+  def decode_to_lowercase(encoded_value) do
+    encoded_value
+    |> String.to_integer()
+    |> to_codepoint_lowercase()
+  end
+
+  defp to_codepoint_lowercase(encoded_value) when rem(encoded_value, 27) == 0 do
+    0
+  end
+
+  defp to_codepoint_lowercase(encoded_value) do
+    rem(encoded_value, 27) + 96
+  end
+
+  def decode_to_punctuation(encoded_value) do
+    encoded_value
+    |> String.to_integer()
+    |> to_codepoint_punctuation()
+  end
+
+  defp to_codepoint_punctuation(encoded_value) when rem(encoded_value, 9) == 0 do
+    0
+  end
+
+  defp to_codepoint_punctuation(encoded_value) do
+    codepoint = rem(encoded_value, 9)
+
+    case codepoint do
+      # !
+      1 -> 33
+      # ?
+      2 -> 63
+      # ,
+      3 -> 44
+      # .
+      4 -> 46
+      # Space
+      5 -> 32
+      # ;
+      6 -> 59
+      # "
+      7 -> 34
+      # '
+      8 -> 39
+    end
+  end
+end
