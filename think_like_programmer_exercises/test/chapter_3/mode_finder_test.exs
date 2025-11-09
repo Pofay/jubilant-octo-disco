7540694028fe7359a649b953ea47f30b2948b62a
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-11-09 13:57:00 +0800

    wip: now able to use reduce as a means to count occurences in the array

--- think_like_programmer_exercises/test/chapter_3/mode_finder_test.exs
+++ think_like_programmer_exercises/test/chapter_3/mode_finder_test.exs
@@ -10,18 +10,25 @@ defmodule ThinkLikeAProgrammerExercises.Chapter2.ModeFinderTest do
     assert actual == expected
   end
 
-  test "Problem Reduction: Using reduce to get the " do
+  test "Problem Reduction: Using reduce to create a list that counts occurences" do
     survey_question_numbers = [4, 4, 1, 2, 4, 5]
-    # Find the index with the highest occurences
     survey_occurences = [0, 0, 0, 0, 0]
     expected = [1, 1, 0, 3, 1]
 
     # Count the occurences by index
+    updated_occurences =
+      Enum.reduce(survey_question_numbers, survey_occurences, fn v, occurences ->
+        case v do
+          0 ->
+            occurences
 
-    updated_occurences = Enum.reduce(survey_question_numbers, survey_occurences, fn v, occurences ->
-      s = Enum.fetch!(survey_question_numbers, v)
-      List.replace_at(occurences, v, s + 1)
-    end)
+          _ ->
+            occurence = Enum.fetch!(occurences, v - 1)
+            List.replace_at(occurences, v - 1, occurence + 1)
+        end
+      end)
+
+    # Find the index with the highest occurences
 
     assert updated_occurences == expected
   end
