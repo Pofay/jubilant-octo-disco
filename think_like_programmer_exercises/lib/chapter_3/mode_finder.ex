9a6e2f1f61882c26cc0667639e848e761eb2ab26
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-11-12 10:14:35 +0800

    feat: Finished Problem 1 of Chapter 3

---
+++ think_like_programmer_exercises/lib/chapter_3/mode_finder.ex
@@ -0,0 +1,17 @@
+defmodule ThinkLikeAProgrammerExercises.Chapter3.ModeFinder do
+  def get_frequency_and_mode(survey_data) do
+    Enum.reduce(survey_data, List.duplicate(0, length(survey_data)), fn questionnaire_index,
+                                                                          histogram ->
+      case questionnaire_index do
+        0 ->
+          histogram
+
+        _ ->
+          frequency = Enum.fetch!(histogram, questionnaire_index - 1)
+          List.replace_at(histogram, questionnaire_index - 1, frequency + 1)
+      end
+    end)
+    |> Enum.with_index(1)
+    |> Enum.max_by(fn {frequency, _possible_mode} -> frequency end)
+  end
+end
--- think_like_programmer_exercises/test/chapter_3/mode_finder_test.exs
+++ think_like_programmer_exercises/test/chapter_3/mode_finder_test.exs
@@ -1,6 +1,8 @@
 defmodule ThinkLikeAProgrammerExercises.Chapter2.ModeFinderTest do
   use ExUnit.Case, async: true
 
+  alias ThinkLikeAProgrammerExercises.Chapter3.ModeFinder
+
   test "Problem Reduction: Checking how replace_at/3 works" do
     list = [0, 0, 0]
     expected = [0, 2, 0]
@@ -10,26 +12,44 @@ defmodule ThinkLikeAProgrammerExercises.Chapter2.ModeFinderTest do
     assert actual == expected
   end
 
-  test "Problem Reduction: Using reduce to create a list that counts occurences" do
+  test "Problem Reduction: Using reduce to create a list that creates histogram via list indexes" do
     survey_question_numbers = [4, 4, 1, 2, 4, 5]
-    survey_occurences = [0, 0, 0, 0, 0]
+    questionnaire_histogram = [0, 0, 0, 0, 0]
     expected = [1, 1, 0, 3, 1]
 
-    # Count the occurences by index
-    updated_occurences =
-      Enum.reduce(survey_question_numbers, survey_occurences, fn v, occurences ->
-        case v do
+    # Count the histogram by index
+    updated_histogram =
+      Enum.reduce(survey_question_numbers, questionnaire_histogram, fn questionnaire_index,
+                                                                       histogram ->
+        case questionnaire_index do
           0 ->
-            occurences
+            histogram
 
           _ ->
-            occurence = Enum.fetch!(occurences, v - 1)
-            List.replace_at(occurences, v - 1, occurence + 1)
+            frequency = Enum.fetch!(histogram, questionnaire_index - 1)
+            List.replace_at(histogram, questionnaire_index - 1, frequency + 1)
         end
       end)
 
-    # Find the index with the highest occurences
+    assert updated_histogram == expected
 
-    assert updated_occurences == expected
+    # Get the mode
+    {frequency, mode} =
+      updated_histogram
+      |> Enum.with_index(1)
+      |> Enum.max_by(fn {frequency, _possible_mode} -> frequency end)
+
+    assert frequency == 3
+    assert mode == 4
+  end
+
+  test "Actual Solution: Get mode and frequency" do
+    survey_data = [7, 7, 7, 2, 3, 2, 10, 5, 6, 6, 7]
+
+    expected = {4, 7}
+
+    actual = ModeFinder.find_mode(survey_data)
+
+    assert actual == expected
   end
 end
